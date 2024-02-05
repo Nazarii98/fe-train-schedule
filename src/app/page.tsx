db49b01d6e9ca65dc8ad19c35bf80688c93b76d6
Nazarii <nazzikk1998@gmail.com>
2024-02-06 00:57:03 +0200

    sort for table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,14 @@ export default function Home() {
     dispatch(fetchSchedule(pagination));
   }, [dispatch, orderDirection, searchValue, page, searchField, orderField]);
 
-  console.log(schedule.schedule);
+  const handleSetSortBy = (value: string) => {
+    if (value === orderField) {
+      setOrderDirection("asc");
+    } else {
+      setOrderField(value);
+      setOrderDirection("desc");
+    }
+  };
 
   return (
     <div className="flex flex-1 flex-col justify-start rounded-3xl bg-gray-800 bg-opacity-90 border-white border-2  overflow-y-auto">
@@ -46,7 +53,10 @@ export default function Home() {
           />
         </div>
       ) : (
-        <ScheduleList schedule={schedule.schedule} />
+        <ScheduleList
+          onSetSort={handleSetSortBy}
+          schedule={schedule.schedule}
+        />
       )}
     </div>
   );
--- src/app/schedule/scheduleList.tsx
+++ src/app/schedule/scheduleList.tsx
@@ -4,20 +4,46 @@ import { ScheduleItem } from "./scheduleItem";
 
 interface Props {
   schedule: Schedule[];
+  onSetSort: (value: string) => void;
 }
 
-
-
-export const ScheduleList: FC<Props> = ({ schedule }) => {
+export const ScheduleList: FC<Props> = ({ schedule, onSetSort }) => {
   return (
     <div>
       <div className="sticky top-0 grid grid-flow-col grid-cols-10 border-b-2 px-20 bg-gray-800">
-        <div className="col-start-1 col-end-2">ID</div>
-        <div className="col-start-2 col-end-4">To</div>
-        <div className="col-start-4 col-end-6">Arrival Date</div>
-        <div className="col-start-6 col-end-8">From</div>
-        <div className="col-start-8 col-end-10 text-center">Departure Time</div>
-        <div className="col-start-10 col-end-11">Train Id</div>
+        <div className="col-start-1 col-end-2" onClick={() => onSetSort("id")}>
+          ID
+        </div>
+        <div
+          className="col-start-2 col-end-4"
+          onClick={() => onSetSort("departureDate")}
+        >
+          To
+        </div>
+        <div
+          className="col-start-4 col-end-6"
+          onClick={() => onSetSort("departureStationId")}
+        >
+          Arrival Date
+        </div>
+        <div
+          className="col-start-6 col-end-8"
+          onClick={() => onSetSort("arrivalDate")}
+        >
+          From
+        </div>
+        <div
+          className="col-start-8 col-end-10 text-center"
+          onClick={() => onSetSort("arrivalStationId")}
+        >
+          Departure Time
+        </div>
+        <div
+          className="col-start-10 col-end-11"
+          onClick={() => onSetSort("itrainId")}
+        >
+          Train Id
+        </div>
       </div>
       {schedule.map((scheduleItem, index) => {
         return <ScheduleItem key={index} schedule={scheduleItem} />;

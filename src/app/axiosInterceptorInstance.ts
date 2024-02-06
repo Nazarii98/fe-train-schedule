19200579ff9d407008e72edd786797f1e231746b
Nazarii <nazzikk1998@gmail.com>
2024-02-06 15:03:31 +0200

    added privat variables to env

--- src/app/axiosInterceptorInstance.ts
+++ src/app/axiosInterceptorInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const API_URL = "http://localhost:4000";
+const API_URL = process.env.NEXT_PUBLIC_BACKEND_URI;
 
 const axiosInterceptorInstance = axios.create({
   baseURL: API_URL,
